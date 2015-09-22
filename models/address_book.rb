e28360114013983b20d6bf9137e736c645cd348b
Onur Sahin <anionrush@gmail.com>
2015-09-22 12:03:50 -0700

    Assignment 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require 'csv'
 
 class AddressBook
 	attr_accessor :entries
@@ -19,5 +20,49 @@ class AddressBook
 
 		@entries.insert(index, Entry.new(name, phone_number, email))
 	end
+
+	def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+ end
+
+ 	def binary_search(name)
+ 		 lower = 0
+     upper = entries.length - 1
+
+     while lower <= upper
+
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+ 		return nil
+ 	end
+
+ 	def iterative_search(name)
+ 		lower = 0
+
+ 		while lower <= entries.length - 1
+ 			if name == entries[lower].name
+ 				return entries[lower]
+ 			else 
+ 				lower += 1
+ 			end
+ 		end
+
+ 		return nil
+ 	end
+ 
 
 end
